9ce220fd9d865b7dc09363825227905bd827e402
RyuseiNomi <kanontonto1010@gmail.com>
2023-10-24 20:16:28 +0900

    Initial Commit

---
+++ CityMusicNavigator/CityMusicNavigatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CityMusicNavigatorApp.swift
+//  CityMusicNavigator
+//
+//  Created by とんと on 2023/10/24.
+//
+
+import SwiftUI
+
+@main
+struct CityMusicNavigatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
